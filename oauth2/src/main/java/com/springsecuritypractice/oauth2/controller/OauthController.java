c53b302e8f758fe61893d2b513c032f930819641
Andras Torok <torokandras1996@gmail.com>
2021-11-02 11:52:38 +0100

    Login now works with Google as well.

--- oauth2/src/main/java/com/springsecuritypractice/oauth2/controller/OauthController.java
+++ oauth2/src/main/java/com/springsecuritypractice/oauth2/controller/OauthController.java
@@ -1,8 +1,11 @@
 package com.springsecuritypractice.oauth2.controller;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +15,16 @@ public class OauthController {
 
     @GetMapping("/user")
     public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) {
-        return Collections.singletonMap("login", principal.getAttribute("login"));
+        Map<String, Object> attributes = new HashMap<>();
+
+        // Google
+        if (principal instanceof DefaultOidcUser) {
+            attributes = Collections.singletonMap("name", principal.getAttribute("name"));
+            // GitHub
+        } else if (principal instanceof DefaultOAuth2User) {
+            attributes = Collections.singletonMap("name", principal.getAttribute("login"));
+        }
+
+        return attributes;
     }
 }
